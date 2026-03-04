7acab67ead6e002900b89249649b716038aefcb5
newtonch66 <chakman828@gmail.com>
2026-03-04 11:00:29 +0600

    lesson api section

--- script/index.js
+++ script/index.js
@@ -1,1 +1,24 @@
-console.log('hello newton')
+const loadLessons =()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then((res) => res.json())
+        .then((json) => displayLesson(json.data));
+};
+
+const displayLesson = (lessons) => {
+    console.log(lessons);
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML ="";
+    for (let lesson of lessons) {
+        console.log(lesson);
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+                               <button class="btn btn-outline btn-primary">
+                               <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+                               </button>
+        `;
+        
+        levelContainer.append(btnDiv);
+    }
+
+}
+loadLessons();
